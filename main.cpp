16219e43c7eefca1882428ce4f336efd2fabc73a
ENDRAAAA <113867834+ENDRAAAA@users.noreply.github.com>
2022-09-26 18:32:33 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int p,l,t;
+    cout <<"masukan panjang balok: ";
+    cin >> p;
+    cout <<"masukan lebar balok: ";
+    cin >> l;
+    cout << "masukan tinggi balok: ";
+    cin >> t;
+
+    cout << endl;
+    cout<<" hasil dari 2(pl+pt+lt)= " <<2*p*l+2*p*t+2*l*3;
+    return 0;
+}
